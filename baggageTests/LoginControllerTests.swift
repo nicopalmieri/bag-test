44ce742ea60c8a0b86a953c5bed1c1c2b3a9956e
Nico Palmieri <nicolas.palmieri@globant.com>
2017-12-18 18:11:23 -0300

    added: quick to the right-target

--- baggageTests/LoginControllerTests.swift
+++ baggageTests/LoginControllerTests.swift
@@ -7,10 +7,35 @@
 //
 
 import Foundation
-import Nimble
 import Quick
+import Nimble
 @testable import baggage
 
 class LoginControllerTests: QuickSpec {
 
+    override func spec() {
+
+        describe("viewDidLoad") {
+
+            // if we have a controller with identifier
+            context("after being initialized") {
+                let identifier = "LoginController"
+//                let bundle = Bundle(for: LoginController.self)
+//                let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+//                let controller = storyboard.instantiateViewController(withIdentifier: identifier)
+
+                it("shouldn't have an identifier") {
+                    // expect(controller).toNot(beNil())
+                }
+            }
+        }
+
+        describe("buttonTapped") {
+            // TUDO
+        }
+
+        describe("setupView") {
+            // TUDO
+        }
+    }
 }
