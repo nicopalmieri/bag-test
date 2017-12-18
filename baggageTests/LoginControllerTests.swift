8903e34d6f9e2af6cc954a0c53bd8373adb469bd
Nico Palmieri <nicolas.palmieri@globant.com>
2017-12-18 17:01:14 -0300

    added: snippets for quick

---
+++ baggageTests/LoginControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  LoginControllerTests.swift
+//  baggageTests
+//
+//  Created by Nicolas Palmieri on 12/18/17.
+//  Copyright © 2017 Nicolas Palmieri. All rights reserved.
+//
+
+import Foundation
+import Nimble
+import Quick
+@testable import baggage
+
+class LoginControllerTests: QuickSpec {
+
+}
